86ebef434aa4383f35e6683bd6a35f68217f3704
leunamenob-ops <leunamenob@gmail.com>
2026-08-08 17:35:16 +0200

    Update page.tsx

--- app/ventas/menu-engineering/menus/page.tsx
+++ app/ventas/menu-engineering/menus/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 export default function VentasHubPage() {
   const router = useRouter();
